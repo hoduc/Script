a3bd721120238fb48f6aa86150922d44e968a884
hoduc <duc_ho90@yahoo.com>
2017-01-11 22:42:25 -0800

    Generating cpp projects

---
+++ NewProjectCpp.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+mkdir -p bin
+mkdir -p build
+mkdir -p doc
+mkdir -p include
+mkdir -p lib
+mkdir -p issue
+mkdir -p src
+mkdir -p test
+
+#generate makefile
+echo "Hello" >> "README.md"
+echo "Hello" >> Makefile
