c948c4af5173d1a068c8d2c98f3f9d3d1dd63f0d
hoduc <duc_ho90@yahoo.com>
2017-09-13 07:03:39 -0400

    Emacs init file

---
+++ init.el
@@ -0,0 +1,168 @@
+;; global variables
+(setq
+ inhibit-startup-screen t
+ create-lockfiles nil
+ make-backup-files nil
+ column-number-mode t
+ scroll-error-top-bottom t
+ show-paren-delay 0.5
+ use-package-always-ensure t
+ sentence-end-double-space nil)
+
+;; buffer local variables
+(setq-default
+ indent-tabs-mode nil
+ tab-width 4
+ c-basic-offset 4)
+
+;; modes
+(electric-indent-mode 0)
+
+;; global keybindings
+(global-unset-key (kbd "C-z"))
+
+;; the package manager
+(require 'package)
+(setq
+ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                    ("org" . "http://orgmode.org/elpa/")
+                    ("melpa" . "http://melpa.org/packages/")
+                    ("melpa-stable" . "http://stable.melpa.org/packages/"))
+ package-archive-priorities '(("melpa-stable" . 1)))
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(use-package ensime
+   :ensure t
+   :pin melpa-stable)
+
+(use-package scala-mode
+   :interpreter
+   ("scala" . scala-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (darcula)))
+ '(custom-safe-themes
+   (quote
+    ("20e23cba00cf376ea6f20049022241c02a315547fc86df007544852c94ab44cb" "cb4cb09f1736cfb2a80ef371c33a9868cbf6707ec5af51d61162483d6c3fbb7c" default)))
+ '(package-selected-packages
+   (quote
+    (nyan-mode helm-descbinds 0blayout ack ibuffer-projectile projectile helm ggtags doom doom-themes xcscope darcula-theme use-package swift3-mode solarized-theme sokoban ensime darkroom avk-emacs-themes auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'xcscope)
+(cscope-setup)
+
+;;helm
+(require 'helm)
+(require 'helm-config)
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "M-x") 'helm-M-x) ;;helm-take-over
+(global-set-key (kbd "M-y") 'helm-show-kill-ring) ;;have not encounter it yet
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+(global-set-key (kbd "C-c h x") 'helm-register)
+(global-set-key (kbd "C-c h g") 'helm-google-suggest) ;search-google
+(global-set-key (kbd "C-c h M-:") 'helm-eval-expression-with-eldoc)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(define-key shell-mode-map (kbd "C-c C-l") 'helm-comint-input-ring)
+(define-key minibuffer-local0map (kbd "C-c C-l") 'helm-minibuffer-history)
+  
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+(when (executable-find "ack-grep")
+  (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
+        helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f"))
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'spacemacs//helm-hide-minibuffer-maybe)
+
+(add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages) ;man pages
+(setq helm-M-x-fuzzy-match t) ;;optional fuzzy matching for helm-M-x
+(setq helm-locate-fuzzy-match t)
+(setq helm-apropos-fuzzy-match t)
+(setq helm-lisp-fuzzy-completion t)
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match t)
+(setq helm-semantic-fuzzy-match t
+      helm-imenu-fuzzy-match t)
+(helm-autoresize-mode t)
+
+(helm-mode 1)
+
+;;projectile
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+
+(setq projectile-switch-project-action 'helm-projectile)
+
+;;helm-eshell
+(require 'helm-eshell)
+(add-hook 'eshell-mode-hook
+          #'(lambda ()
+              (define-key eshell-mode-map (kbd "C-c C-l") 'helm-eshell-history)))
+
+;; helm-descbinds
+(require 'helm-descbinds)
+(helm-descbinds-mode)
+
+;; Steve Yege suggestion
+;; https://sites.google.com/site/steveyegge2/effective-emacs
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+;;emacswikiemacs 
+(global-set-key (kbd "RET") 'reindent-then-newline-and-indent)
+;;
+;;no toolbar, scrollbar, menubar
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;;eshell color scheme
+(add-hook 'eshell-preoutput-filter-functionss
+          'ansi-color-filter-apply)
